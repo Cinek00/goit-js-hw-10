0e407f39ff1cfaf6bf90584f2a488424ad3ee4a6
Cinek00 <marcin.brzegowy00@gmail.com>
2024-01-12 15:34:24 +0100

    del

--- src/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-export default function fetchCountries(name) {
-  const url = `https://restcountries.eu/v2/name/${name}?fields=name.official,capital,population,flags.svg,languages`;
-  return fetch(url)
-    .then(response => response.json())
-    .catch(error => {
-      console.error(error);
-    });
-}
